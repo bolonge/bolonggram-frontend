f475418648b640c9cf2e12baebdfe0ba6d1d1f6e
bolonge <xxs456@naver.com>
2019-05-17 23:52:53 +0900

    setup

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "1px soild #e6e6e6";
+const BORDER_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  blueColor: "#3897f0",
+  redColor: "#ED4956",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  darkBlueColor: "#003569",
+  boxBorder: "1px soild #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BORDER_RADIUS};
+            ${BOX_BORDER};
+            background-color:white;
+            `
+};
